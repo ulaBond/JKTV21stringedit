29dc3b2c79b4527d48bc5e0cb4968832b72cee13
pupil <pupil@ivkhk.ee>
2022-09-21 09:26:41 +0300

    second commit

---
+++ src/jktv21stringedit/JKTV21stringedit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv21stringedit;
+
+/**
+ *
+ * @author pupil
+ */
+public class JKTV21stringedit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Строки в Java");
+    }
+    
+}
